4622092941f518d42b0410ad8f461f80d22a5c0a
Blaster1111 <rudrashukla0711@gmail.com>
2023-09-19 14:09:49 +0530

    Expert gpt

--- lib/AuthActivity/expert_details.dart
+++ lib/AuthActivity/expert_details.dart
@@ -8,11 +8,53 @@ class ExpertDetails extends StatefulWidget {
 }
 
 class _ExpertDetailsState extends State<ExpertDetails> {
+  final _nameController = TextEditingController();
+  final _expController = TextEditingController();
+  final _qualificationController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _aadharController = TextEditingController();
+  final _expertiseController = TextEditingController();
+
+  bool isFormValid = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController.addListener(validateForm);
+    _expController.addListener(validateForm);
+    _qualificationController.addListener(validateForm);
+    _emailController.addListener(validateForm);
+    _aadharController.addListener(validateForm);
+    _expertiseController.addListener(validateForm);
+  }
+
+  void validateForm() {
+    setState(() {
+      isFormValid = _nameController.text.isNotEmpty &&
+          _expController.text.isNotEmpty &&
+          _qualificationController.text.isNotEmpty &&
+          _emailController.text.isNotEmpty &&
+          _aadharController.text.isNotEmpty &&
+          _expertiseController.text.isNotEmpty;
+    });
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _expController.dispose();
+    _qualificationController.dispose();
+    _emailController.dispose();
+    _aadharController.dispose();
+    _expertiseController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.grey.shade300, // Set the background color here
+        color: Colors.grey.shade300,
         child: Center(
           child: SingleChildScrollView(
             padding: EdgeInsets.all(16.0),
@@ -27,25 +69,41 @@ class _ExpertDetailsState extends State<ExpertDetails> {
                   ),
                 ),
                 SizedBox(height: 20.0),
-                _buildCircularTextField("Name"),
+                _buildCircularTextField(_nameController, "Name"),
                 SizedBox(height: 10.0),
-                _buildCircularTextField("Years of Experience"),
+                _buildCircularTextField(_expController, "Years of Experience"),
                 SizedBox(height: 10.0),
-                _buildCircularTextField("Qualification"),
+                _buildCircularTextField(
+                    _qualificationController, "Qualification"),
                 SizedBox(height: 10.0),
-                _buildCircularTextField("Email"),
+                _buildCircularTextField(_emailController, "Email"),
                 SizedBox(height: 10.0),
-                _buildCircularTextField("Aadhar Number"),
+                _buildCircularTextField(_aadharController, "Aadhar Number"),
                 SizedBox(height: 10.0),
-                _buildCircularTextField("Expertise"),
+                _buildCircularTextField(_expertiseController, "Expertise"),
                 SizedBox(height: 20.0),
                 ElevatedButton(
-                  onPressed: () {
-                    // Handle verification logic here
-                  },
+                  onPressed: isFormValid
+                      ? () {
+                          // Handle verification logic here
+                          String name = _nameController.text;
+                          String exp = _expController.text;
+                          String qualification = _qualificationController.text;
+                          String email = _emailController.text;
+                          String aadhar = _aadharController.text;
+                          String expertise = _expertiseController.text;
+
+                          // Use the captured values as needed
+                          print("Name: $name");
+                          print("Experience: $exp");
+                          print("Qualification: $qualification");
+                          print("Email: $email");
+                          print("Aadhar Number: $aadhar");
+                          print("Expertise: $expertise");
+                        }
+                      : null,
                   style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        Colors.green.shade400, // Set the button color here
+                    backgroundColor: Colors.green.shade400,
                   ),
                   child: Text("Verify Details"),
                 ),
@@ -57,13 +115,15 @@ class _ExpertDetailsState extends State<ExpertDetails> {
     );
   }
 
-  Widget _buildCircularTextField(String labelText) {
+  Widget _buildCircularTextField(
+      TextEditingController controller, String labelText) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30.0),
         color: Colors.white,
       ),
       child: TextField(
+        controller: controller,
         decoration: InputDecoration(
           labelText: labelText,
           border: InputBorder.none,
